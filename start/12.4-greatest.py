6ad784dea33bc3b5af75dcfa89df56a1f39340d9
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-07-17 08:06:32 -0600

    add greatest challenge

---
+++ start/12.4-greatest.py
@@ -0,0 +1,15 @@
+# Define a procedure, greatest,
+# that takes as input a list
+# of positive numbers, and
+# returns the greatest number
+# in that list. If the input
+# list is empty, the output
+# should be 0.
+
+
+
+print greatest([4,23,1])
+#>>> 23
+
+print greatest([])
+#>>> 0
